b5e6ef4298c04dd2a7248e25a17d913c7a7eccf7
tuturuuu <viettpm26@gmail.com>
2024-04-17 18:10:16 +0700

    with color

---
+++ components/layouts/main.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Navbar from '../navbar'
+import { Box, Container } from '@chakra-ui/react'
+
+const Main = ({ children, router }) => {
+    return (
+      <Box as="main" pb={8}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Pham Minh Viet - Homepage</title>
+        </Head>
+  
+        <Navbar />
+  
+        <Container maxW="" pt={14}>
+  
+          {children}
+  
+        </Container>
+      </Box>
+    )
+  }
+  
+  export default Main
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../libs/theme";
-import Navbar from "../components/navbar";
-import Chakra from '../components/chakra'
+import Layout from '../components/layouts/main'
 
-function MyApp({ Component, pageProps }) {
+function Website({ Component, pageProps, router }) {
+
   return (
-      <Chakra cookies={pageProps.cookies}>
-      <Navbar />
-      <Component {...pageProps}/>
-      </Chakra>
+    
+    <ChakraProvider theme={theme}>
+      <Layout router={router}>
+          <Component {...pageProps} key={router.route} />
+     </Layout>
+
+    </ChakraProvider>
   );
 }
 
-export default MyApp;
+export default Website;
