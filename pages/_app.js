86ef37cee2b8eb0a620150a2f99a2f5f0bd458db
tuturuuu <viettpm26@gmail.com>
2024-04-17 16:51:15 +0700

    with color

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../libs/theme";
 import Navbar from "../libs/ui/fragments/navbar";
+import Chakra from '../components/chakra'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider theme={theme}>
+      <Chakra cookies={pageProps.cookies}>
       <Navbar />
-      <Component {...pageProps} />
-    </ChakraProvider>
+      <Component {...pageProps}/>
+      </Chakra>
   );
 }
 
